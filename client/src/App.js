d7c5284eb08967f8098c556cbfdf0e6f57c4f355
oyeon-kwon <rnjsdhdus93@gmail.com>
2022-05-04 15:16:14 +0900

    Add: 랜딩페이지, 네비게이션 바 완성

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import { firestore } from "./firebase";
+import { firestore } from './firebase';
+import Landing from './Landing';
+import Nav from './Nav';
+
 
 function App() {
-  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='body'>
+      <Nav />
+      <Landing />
     </div>
   );
 }
---
+++ client/src/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './nav.css';
+
+function Nav () {
+  
+    return (
+        <>
+        <div className='nav'>
+          <span className='nav-link'>HOME</span>
+          <span className='nav-link'>COLOR</span>
+          <span className='nav-link'>COMMUNITY</span>
+          <span className='nav-link'>MYPAGE</span>
+        </div>
+        </>
+    )
+}
+
+export default Nav;
