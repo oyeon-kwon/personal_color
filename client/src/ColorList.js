d3d77398d588a1d58355be50614d82752bc72d58
oyeon-kwon <rnjsdhdus93@gmail.com>
2022-05-30 21:10:26 +0900

    Fix: 컬러 리스트 Tab view로 수정

--- client/src/ColorList.js
+++ client/src/ColorList.js
@@ -4,17 +4,40 @@ import colorData from './color.json';
 import './colorlist.css';
 
 function ColorList () {
-    // TODO: 
+  // TODO: color.json 데이터 가공 필요 (봄 여름 가을 겨울 카테고리로 나누기)
+  const [currentTab, setCurrentTab] = useState(0);
 
-  const colorThemes = Object.keys(colorData)
-  console.log(colorThemes)
+  const seasons = [
+      { name: "전체", colorThemes: Object.keys(colorData)},
+      { name: "봄", colorThemes: ['spring-light-1', 'spring-light-2', 'spring-bright']},
+      { name: "여름", colorThemes: ['summer-light', 'summer-light-bright', 'summer-bright', 'summer-mute-1', 'summer-mute-2', 'summer-mute-3', 'summer-mute-4']}
+  ]
+
+  const selectMenuHandler = (index) => {
+    setCurrentTab(index);
+  };
 
   return (
     <>
-    <div className="color-list">
+    <div className='tab-box'>
         {
-            colorThemes.map((colorTheme) => {
-                return <Color colorTheme={colorTheme} key={colorTheme} />
+            seasons.map((season, index) => {
+                return (
+                    <li
+                        key={index}
+                        className={currentTab === index ? 'submenu focused' : 'submenu'}
+                        onClick={() => selectMenuHandler(index)}
+                    >
+                    {season.name}
+                    </li>
+                )
+            })
+        }
+    </div>
+    <div className='color-list'>
+        {
+            seasons[currentTab].colorThemes.map((colorTheme, index) => {
+                return <Color colorTheme={colorTheme} key={index} />
             })
         }
     </div>
