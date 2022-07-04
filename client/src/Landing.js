1483b74e08b5fed08267828f60c1c7b1ab954b1a
oyeon-kwon <rnjsdhdus93@gmail.com>
2022-07-04 18:06:05 +0900

    Add: 랜딩페이지 애니메이션 작업 진행 중

--- client/src/Landing.js
+++ client/src/Landing.js
@@ -1,11 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import './landing.css';
 import selfImg from './img/self.svg';
 import aiImg from './img/ai.svg';
 import { useNavigate } from 'react-router-dom';
+import { Controller, Scene } from 'react-scrollmagic';
+// import ScrollMagic from 'scrollmagic';
+import { Controls, PlayState, Reveal, Timeline, Tween, SplitWords } from 'react-gsap';
+import arrow from './img/arrow.png'
 
 function Landing () {
   // TODO: 호버 이미지 크기 조정
+  const ref = useRef();
   const navigate = useNavigate();
   const [buttonStatus, setButtonStatus] = useState('self');
 
@@ -31,30 +36,109 @@ function Landing () {
       </div>
       <div className='img-container'>
         {
-                buttonStatus === 'self'
-                  ? <img src={selfImg} className='button-img' alt='img' />
-                  : buttonStatus === 'ai' ? <img src={aiImg} className='button-img' alt='img' />
-                    : <></>
-            }
+          buttonStatus === 'self'
+            ? <img src={selfImg} className='button-img' alt='img' />
+            : buttonStatus === 'ai' ? <img src={aiImg} className='button-img' alt='img' />
+              : <></>
+        }
+      </div>
+      
+      <div className='landing-section'>
+        <div className='arrow-container'>
+          <div className='arrow-container-desc'>퍼스널 컬러가 ?</div>
+          <img src={arrow} alt='' className='arrow-img'/>
+        </div>
+        <div className='landing-section-1'>
+        <Controller>
+          <Scene
+            pin={false}
+            reverse={true}
+            duration={500}
+            offset={-300}        
+          >
+            <Timeline target={<div className='person-thinking'>내 얼굴 톤에 맞는 화장품이 뭔지 모르겠어요.<img src='https://mblogthumb-phinf.pstatic.net/20151005_163/yaho_17_1444057061878SKfrr_PNG/%C0%CC%C0%C0.png?type=w2' className='landing-section1-img'/></div>}>
+              <Tween from={{ opacity: 0 }} to={{ opacity: 1 }} duration={1} />
+              <Tween to={{ x: "50px", y: "200px" }} duration={1} />
+            </Timeline>
+          </Scene>
+        </Controller>
+
+        <Controller>
+          <Scene
+            pin={false}
+            reverse={true}
+            duration={500}
+            offset={-300}        
+          >
+            <Timeline target={<div className='person-thinking'>톤그로라고 놀림 받았어요ㅠㅠ</div>}>
+              <Tween from={{ opacity: 0 }} to={{ opacity: 1 }} duration={1} />
+              <Tween to={{ x: "400px", y: "50px" }} duration={1} />
+            </Timeline>
+          </Scene>
+        </Controller>
+
+        <Controller>
+          <Scene
+            pin={false}
+            reverse={true}
+            duration={500}
+            offset={-300}        
+          >
+            <Timeline target={<div className='person-thinking'>핑크립을 바르면 왜 이렇게 입술만 둥둥 떠보일까요?</div>}>
+              <Tween from={{ opacity: 0 }} to={{ opacity: 1 }} duration={1} />
+              <Tween to={{ x: "600px", y: "180px" }} duration={1} />
+            </Timeline>
+          </Scene>
+        </Controller>
+        </div>
+
+        <div className='landing-section-2'>
+          <Controller>
+            <Scene
+              duration="70%"
+              triggerHook="onEnter"
+            >
+                {/* <Tween
+                  position="0"
+                  from={{
+                    yPercent: -50,
+                  }}
+                  to={{
+                      yPercent: 0,
+                  }}
+                >
+                  <img src="https://placeimg.com/1000/1000/nature" alt="" />
+                </Tween> */}
+                <Tween
+                position="0"
+                  from={{
+                    top: '0%',
+                    scale: 0.5,
+                  }}
+                  to={{
+                    top: '10%',
+                    scale: 1,
+                  }}
+                >
+                  <div className='section-container'>
+                    <div className='landing-section-2-text'>스스로 여러가지 색상표를 바꿔보며 <br /> 내 얼굴이 환하고 생기있어 보이는 색이 <br /> 무엇인지 골라보세요.</div>
+                    <img src="https://placeimg.com/1000/1000/nature" className='landing-section-2-img' alt="" />
+                  </div>
+                </Tween>
+            </Scene>
+          </Controller>
+        </div>
+        <div className='landing-section-3'>
+          빅데이터 기반으로 학습한 AI를 토대로 내 퍼스널 컬러 자동 진단을 받아보세요.
+        </div>
+        <div className='landing-section-4'>
+          나에게 어울리는 색상이 무엇인지, 내 착붙템은 무엇일지 한 눈에 살펴보세요.
+        </div>
+        <div className='landing-section-5'>
+          나와 같은 톤인 사람들과 함께 메이크업, 의상 등 다양한 정보를 공유하세요.
+        </div>
       </div>
 
-      <div>
-        내 얼굴 톤에 맞는 화장품이 뭔지 모르겠어요.
-        톤그로라고 놀림 받았어요ㅠㅠ
-        핑크립을 바르면 왜 이렇게 입술만 둥둥 떠보일까요?
-      </div>
-      <div>
-        스스로 여러가지 색상표를 바꿔보며 내 얼굴이 환하고 생기있어 보이는 색이 무엇인지 골라보세요.
-      </div>
-      <div>
-        빅데이터 기반으로 학습한 AI를 토대로 내 퍼스널 컬러 자동 진단을 받아보세요.
-      </div>
-      <div>
-        나에게 어울리는 색상이 무엇인지, 내 착붙템은 무엇일지 한 눈에 살펴보세요.
-      </div>
-      <div>
-        나와 같은 톤인 사람들과 함께 메이크업, 의상 등 다양한 정보를 공유하세요.
-      </div>
     </>
   );
 }
