ed0355c66cf9617eb2685cf464fd9f575581e01b
oyeon-kwon <rnjsdhdus93@gmail.com>
2022-07-06 22:45:33 +0900

    Add: 랜딩페이지 이미지 삽입

--- client/src/Landing.js
+++ client/src/Landing.js
@@ -4,9 +4,14 @@ import selfImg from './img/self.svg';
 import aiImg from './img/ai.svg';
 import { useNavigate } from 'react-router-dom';
 import { Controller, Scene } from 'react-scrollmagic';
-// import ScrollMagic from 'scrollmagic';
 import { Controls, PlayState, Reveal, Timeline, Tween, SplitWords } from 'react-gsap';
 import arrow from './img/arrow.png'
+import landingFirst1 from './img/landing1.png'
+import landingFirst2 from './img/landing2.png'
+import landingFirst3 from './img/landing3.png'
+import browser from './img/browser.png'
+import landingSecondGIF from './img/landingSecondGIF.gif'
+import landingThirdGIF from './img/landingThirdGIF.gif'
 
 function Landing () {
   // TODO: 호버 이미지 크기 조정
@@ -45,7 +50,7 @@ function Landing () {
       
       <div className='landing-section'>
         <div className='arrow-container'>
-          <div className='arrow-container-desc'>퍼스널 컬러가 ?</div>
+          <div className='arrow-container-desc'>퍼스널 컬러를 알면 뭐가 좋나요?</div>
           <img src={arrow} alt='' className='arrow-img'/>
         </div>
         <div className='landing-section-1'>
@@ -53,12 +58,12 @@ function Landing () {
           <Scene
             pin={false}
             reverse={true}
-            duration={500}
+            duration={300}
             offset={-300}        
           >
-            <Timeline target={<div className='person-thinking'>내 얼굴 톤에 맞는 화장품이 뭔지 모르겠어요.<img src='https://mblogthumb-phinf.pstatic.net/20151005_163/yaho_17_1444057061878SKfrr_PNG/%C0%CC%C0%C0.png?type=w2' className='landing-section1-img'/></div>}>
+            <Timeline target={<div className='person-thinking'>내 얼굴 톤에 맞는 화장품이 뭔지 모르겠어요.<img src={landingFirst1} className='landing-section1-img1'/></div>}>
               <Tween from={{ opacity: 0 }} to={{ opacity: 1 }} duration={1} />
-              <Tween to={{ x: "50px", y: "200px" }} duration={1} />
+              <Tween to={{ x: "0px", y: "150px" }} duration={1} />
             </Timeline>
           </Scene>
         </Controller>
@@ -67,12 +72,12 @@ function Landing () {
           <Scene
             pin={false}
             reverse={true}
-            duration={500}
+            duration={300}
             offset={-300}        
           >
-            <Timeline target={<div className='person-thinking'>톤그로라고 놀림 받았어요ㅠㅠ</div>}>
+            <Timeline target={<div className='person-thinking'>톤그로라고 놀림 받았어요ㅠㅠ<img src={landingFirst2} className='landing-section1-img2'/></div>}>
               <Tween from={{ opacity: 0 }} to={{ opacity: 1 }} duration={1} />
-              <Tween to={{ x: "400px", y: "50px" }} duration={1} />
+              <Tween to={{ x: "30px", y: "0px" }} duration={1} />
             </Timeline>
           </Scene>
         </Controller>
@@ -81,12 +86,12 @@ function Landing () {
           <Scene
             pin={false}
             reverse={true}
-            duration={500}
+            duration={300}
             offset={-300}        
           >
-            <Timeline target={<div className='person-thinking'>핑크립을 바르면 왜 이렇게 입술만 둥둥 떠보일까요?</div>}>
+            <Timeline target={<div className='person-thinking'>핑크립을 바르면 왜 이렇게 입술만 둥둥 떠보일까요?<img src={landingFirst3} className='landing-section1-img3'/></div>}>
               <Tween from={{ opacity: 0 }} to={{ opacity: 1 }} duration={1} />
-              <Tween to={{ x: "600px", y: "180px" }} duration={1} />
+              <Tween to={{ x: "30px", y: "150px" }} duration={1} />
             </Timeline>
           </Scene>
         </Controller>
@@ -98,18 +103,7 @@ function Landing () {
               duration="70%"
               triggerHook="onEnter"
             >
-                {/* <Tween
-                  position="0"
-                  from={{
-                    yPercent: -50,
-                  }}
-                  to={{
-                      yPercent: 0,
-                  }}
-                >
-                  <img src="https://placeimg.com/1000/1000/nature" alt="" />
-                </Tween> */}
-                <Tween
+              <Tween
                 position="0"
                   from={{
                     top: '0%',
@@ -121,22 +115,70 @@ function Landing () {
                   }}
                 >
                   <div className='section-container'>
-                    <div className='landing-section-2-text'>스스로 여러가지 색상표를 바꿔보며 <br /> 내 얼굴이 환하고 생기있어 보이는 색이 <br /> 무엇인지 골라보세요.</div>
-                    <img src="https://placeimg.com/1000/1000/nature" className='landing-section-2-img' alt="" />
+                    <div className='landing-section-text'>스스로 여러가지 색상표를 바꿔보며 <br /> 내 얼굴이 환하고 생기있어 보이는 색이 <br /> 무엇인지 골라보세요.</div>
+                    <img src={browser} className='landing-section-browser-img' alt="" />
+                    <img src={landingSecondGIF} className='gif' alt="" />
                   </div>
-                </Tween>
+                 </Tween>
             </Scene>
           </Controller>
         </div>
-        <div className='landing-section-3'>
-          빅데이터 기반으로 학습한 AI를 토대로 내 퍼스널 컬러 자동 진단을 받아보세요.
+        <div className='landing-section-2'>
+        <Controller>
+            <Scene
+              duration="70%"
+              triggerHook="onEnter"
+            >
+              <Tween
+                position="0"
+                  from={{
+                    top: '0%',
+                    scale: 0.5,
+                  }}
+                  to={{
+                    top: '10%',
+                    scale: 1,
+                  }}
+                >
+                  <div className='section-container'>
+                    <div className='landing-section-text'>빅데이터 기반으로 학습한 AI를 토대로 <br /> 내 퍼스널 컬러 자동 진단을 받아보세요.</div>
+                    <img src={browser} className='landing-section-browser-img' alt="" />
+                    <img src={landingThirdGIF} className='gif' alt="" />
+                  </div>
+              </Tween>
+            </Scene>
+          </Controller>
         </div>
-        <div className='landing-section-4'>
+
+        <div className='landing-section-2'>
+          <Controller>
+            <Scene
+              duration="70%"
+              triggerHook="onEnter"
+            >
+              <Tween
+                position="0"
+                  from={{
+                    top: '0%',
+                    scale: 0.5,
+                  }}
+                  to={{
+                    top: '10%',
+                    scale: 1,
+                  }}
+                >
+                  <div className='section-container'>
+                    <div className='landing-section-text'>나와 같은 톤인 사람들과 함께 <br /> 메이크업, 의상 등 다양한 정보를 공유하세요.</div>
+                    <img src={browser} className='landing-section-browser-img' alt="" />
+                    <img src={landingThirdGIF} className='gif' alt="" />
+                  </div>
+              </Tween>
+            </Scene>
+          </Controller>
+        </div>
+        {/* <div className='landing-section-2'>
           나에게 어울리는 색상이 무엇인지, 내 착붙템은 무엇일지 한 눈에 살펴보세요.
-        </div>
-        <div className='landing-section-5'>
-          나와 같은 톤인 사람들과 함께 메이크업, 의상 등 다양한 정보를 공유하세요.
-        </div>
+        </div> */}
       </div>
 
     </>
