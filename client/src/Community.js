6650da60685160cbbb00131278d42f87bbb6e910
oyeon-kwon <rnjsdhdus93@gmail.com>
2023-10-29 16:27:48 +0900

    remove Kakao login

--- client/src/Community.js
+++ client/src/Community.js
@@ -1,56 +1,58 @@
-import React, { useState, useRef, useEffect, useMemo } from 'react';
-import Pagination from './components/Pagination';
-import './community.css';
-import { getAllPostsData, getFilteredByCategoryPostsData } from './firebase/firebase';
-import { useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import React, { useState, useRef, useEffect, useMemo } from "react";
+import Pagination from "./components/Pagination";
+import "./community.css";
+import {
+  getAllPostsData,
+  getFilteredByCategoryPostsData,
+} from "./firebase/firebase";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
-function Community () {
+function Community() {
   const navigate = useNavigate();
   // 리덕스에 저장된 authCurrentUser의 정보
   const authCurrentUser = useSelector((state) => state.authReducer.auth);
 
   const [postsData, setPostData] = useState([]);
   const [currentTab, setCurrentTab] = useState(0);
-  const categories = ['전체', 'WARM', 'COOL', '모르겠어요'];
+  const categories = ["전체", "WARM", "COOL", "모르겠어요"];
 
   // 페이지네이션에 필요한 상태
   const limit = 10;
   const [page, setPage] = useState(1);
   const offset = (page - 1) * limit;
 
-
   const getAllPosts = async () => {
     const response = await getAllPostsData();
     const allPostArray = [];
 
-    
     for (const key in response) {
       const postObject = {
         id: key,
-        ...response[key]
+        ...response[key],
       };
       allPostArray.push(postObject);
     }
 
     let orderedByRecentPost = allPostArray.sort((a, b) => {
-      return new Date(b.createdAt) - new Date(a.createdAt)
-    })
+      return new Date(b.createdAt) - new Date(a.createdAt);
+    });
 
-    setPostData(orderedByRecentPost)
+    setPostData(orderedByRecentPost);
   };
-  
+
   const selectCategoryHandler = async (index) => {
     setCurrentTab(index);
-    
+
     if (index === 0) {
       return getAllPosts();
     }
-    const filteredByCategoryData = await getFilteredByCategoryPostsData(categories[index]);
-    setPage(1)
+    const filteredByCategoryData = await getFilteredByCategoryPostsData(
+      categories[index]
+    );
+    setPage(1);
     setPostData(filteredByCategoryData);
   };
-
 
   const viewPostHandler = (e) => {
     const id = e.target.dataset.key;
@@ -63,70 +65,74 @@ function Community () {
   };
 
   const postDataRenderer = (post) => {
+    if (!post) {
+      return <div>로딩 중</div>;
+    }
     return (
-      <tr className='community-content' data-id={post.id} data-key={post.id} onClick={viewPostHandler}>
-        <td className='community-content-category' data-key={post.id}>{post.category}</td>
-        <td className='community-content-title' data-key={post.id}>{post.title}</td>
-        <td className='community-content-date' data-key={post.id}>{post.createdAt}</td>
+      <tr
+        className="community-content"
+        data-id={post.id}
+        data-key={post.id}
+        onClick={viewPostHandler}
+      >
+        <td className="community-content-category" data-key={post.id}>
+          {post.category}
+        </td>
+        <td className="community-content-title" data-key={post.id}>
+          {post.title}
+        </td>
+        <td className="community-content-date" data-key={post.id}>
+          {post.createdAt}
+        </td>
       </tr>
     );
   };
 
-
   const writePostHandler = () => {
     if (authCurrentUser) {
       // 글쓰기 가능
-      navigate('/community/post');
+      navigate("/community/post");
     } else {
       // 글쓰기 불가
-      alert('로그인이 필요한 서비스입니다.');
-      navigate('/signin');
+      alert("로그인이 필요한 서비스입니다.");
+      navigate("/signin");
     }
   };
 
-    
   useEffect(() => {
     getAllPosts();
   }, []);
 
-
   return (
     <>
-      <div className='category-box'>
-        {
-            categories.map((category, index) => {
-              return (
-                <div
-                  key={index}
-                  className={currentTab === index ? 'category focused' : 'category'}
-                  onClick={() => selectCategoryHandler(index)}
-                >
-                  {category}
-                </div>
-              );
-            })
-        }
+      <div className="category-box">
+        {categories.map((category, index) => {
+          return (
+            <div
+              key={index}
+              className={currentTab === index ? "category focused" : "category"}
+              onClick={() => selectCategoryHandler(index)}
+            >
+              {category}
+            </div>
+          );
+        })}
       </div>
-      {/* <div className='search-box'>
-        <input placeholder='검색어를 입력하세요' />
-        TODO: 아이콘
-        <button>검색</button>
-      </div> */}
-      <div className='community-body'>
-        <button className='write-post-button' onClick={writePostHandler}>글쓰기</button>
+      <div className="community-body">
+        <button className="write-post-button" onClick={writePostHandler}>
+          글쓰기
+        </button>
 
-        <div className='community-content-box'>
-          {
-            postsData.length === 0
-              ? <div>게시물이 없습니다.</div>
-              : <table>
-                <tbody>
-                  {
-                    postsData.slice(offset, offset + limit).map(postDataRenderer)
-                  }
-                </tbody>
-              </table>
-          }
+        <div className="community-content-box">
+          {postsData.length === 0 ? (
+            <div>게시물이 없습니다.</div>
+          ) : (
+            <table>
+              <tbody>
+                {postsData.slice(offset, offset + limit).map(postDataRenderer)}
+              </tbody>
+            </table>
+          )}
           <footer>
             <Pagination
               total={postsData.length}
--- client/src/Login.js
+++ client/src/Login.js
@@ -1,19 +1,25 @@
-import React, { useState, useEffect } from 'react';
-import './login.css';
-import { auth, loginEmail, getUserData, writeUserData, verifyTokenFromFirebase, getCurrentLoggedInUser } from './firebase/firebase';
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
-import { useNavigate } from 'react-router-dom';
-import { setAuth } from './reducer/authReducer';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useState, useEffect } from "react";
+import "./login.css";
+import {
+  auth,
+  loginEmail,
+  getUserData,
+  writeUserData,
+  verifyTokenFromFirebase,
+  getCurrentLoggedInUser,
+} from "./firebase/firebase";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import { setAuth } from "./reducer/authReducer";
+import { useDispatch, useSelector } from "react-redux";
 /* global Kakao */
 
-function Login () {
-
+function Login() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  const [emailInput, setEmailInput] = useState('');
-  const [passwordInput, setPasswordInput] = useState('');
+  const [emailInput, setEmailInput] = useState("");
+  const [passwordInput, setPasswordInput] = useState("");
 
   const emailInputHandler = (e) => {
     const email = e.target.value;
@@ -28,26 +34,25 @@ function Login () {
   const submitLoginHandler = async () => {
     try {
       const userinfo = await loginEmail(emailInput, passwordInput);
-      getUserData(userinfo.user.uid)
-        .then((snapshot) => {
-          if (snapshot.exists()) {
-            alert('로그인에 성공했습니다.');
-            navigate('/');
-            window.location.reload();
-            // verifyTokenFromFirebase();
-          } else {
-            console.log('No data available');
-          }
-        });
+      getUserData(userinfo.user.uid).then((snapshot) => {
+        if (snapshot.exists()) {
+          alert("로그인에 성공했습니다.");
+          navigate("/");
+          window.location.reload();
+          // verifyTokenFromFirebase();
+        } else {
+          console.log("No data available");
+        }
+      });
     } catch (err) {
-      if (err.message === 'Firebase: Error (auth/user-not-found).') {
-        alert('이메일에 해당하는 유저를 찾을 수 없습니다.');
+      if (err.message === "Firebase: Error (auth/user-not-found).") {
+        alert("이메일에 해당하는 유저를 찾을 수 없습니다.");
       }
-      if (err.message === 'Firebase: Error (auth/wrong-password).') {
-        alert('비밀번호가 올바르지 않습니다.');
+      if (err.message === "Firebase: Error (auth/wrong-password).") {
+        alert("비밀번호가 올바르지 않습니다.");
       }
-      if (err.message === 'Firebase: Error (auth/invalid-email).') {
-        alert('이메일 형식이 유효하지 않습니다.');
+      if (err.message === "Firebase: Error (auth/invalid-email).") {
+        alert("이메일 형식이 유효하지 않습니다.");
       }
     }
   };
@@ -55,55 +60,75 @@ function Login () {
   const googleLoginHandler = async () => {
     const googleProvider = new GoogleAuthProvider();
 
-    googleProvider.addScope('profile');
-    googleProvider.addScope('email');
+    googleProvider.addScope("profile");
+    googleProvider.addScope("email");
     const googleLoginResult = await signInWithPopup(auth, googleProvider);
-    
-    // 유저 정보 
+
+    // 유저 정보
     const user = {
       userId: googleLoginResult.user.uid,
       username: googleLoginResult.user.displayName,
       email: googleLoginResult.user.email,
-      image: googleLoginResult.user.photoURL
-    }
+      image: googleLoginResult.user.photoURL,
+    };
 
     dispatch(setAuth(user));
-    navigate('/');
+    navigate("/");
 
     // 액세스 토큰
-    const credential = GoogleAuthProvider.credentialFromResult(googleLoginResult);
+    const credential =
+      GoogleAuthProvider.credentialFromResult(googleLoginResult);
     const token = credential.accessToken;
   };
 
   const kakaoLoginHandler = () => {
     Kakao.Auth.authorize({
-      redirectUri: 'http://localhost:3000'
+      redirectUri: "http://localhost:3000",
     });
   };
 
   return (
     <>
-      <div className='modal-background'>
-        <div className='modal-body'>
-          <div className='login-title'>Login with</div>
-          <div className='form'>
-            <div className='email'>
-              <div className='desc'>email</div>
-              <input className='login-email-form-box' onChange={emailInputHandler} />
+      <div className="modal-background">
+        <div className="modal-body">
+          <div className="login-title">Login with</div>
+          <div className="form">
+            <div className="email">
+              <div className="desc">email</div>
+              <input
+                className="login-email-form-box"
+                onChange={emailInputHandler}
+              />
             </div>
-            <div className='password'>
-              <div className='desc'>비밀번호</div>
-              <input className='login-password-form-box' type='password' onChange={passwordInputHandler} />
+            <div className="password">
+              <div className="desc">비밀번호</div>
+              <input
+                className="login-password-form-box"
+                type="password"
+                onChange={passwordInputHandler}
+              />
             </div>
-            <div className='submit-button' onClick={submitLoginHandler}>로그인</div>
-            <div className='hr'>
-              <span className='login-line' />
+            <div className="submit-button" onClick={submitLoginHandler}>
+              로그인
+            </div>
+            <div className="hr">
+              <span className="login-line" />
               <span>or</span>
-              <span className='login-line' />
+              <span className="login-line" />
             </div>
-            <div className='social-login-box'>
-              <span className='google social-login-button' onClick={googleLoginHandler}>구글</span>
-              <span className='kakao social-login-button' onClick={kakaoLoginHandler}>카카오</span>
+            <div className="social-login-box">
+              <span
+                className="google social-login-button"
+                onClick={googleLoginHandler}
+              >
+                구글
+              </span>
+              {/* <span
+                className="kakao social-login-button"
+                onClick={kakaoLoginHandler}
+              >
+                카카오
+              </span> */}
             </div>
           </div>
         </div>
